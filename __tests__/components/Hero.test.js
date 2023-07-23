ae8f6dc798a207c3812bedd2be47e716480e4bfa
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-07-23 15:17:38 +0600

    fixed the test

--- __tests__/components/Hero.test.js
+++ __tests__/components/Hero.test.js
@@ -8,6 +8,6 @@ describe("the hero section", () => {
 
     const headingEl = screen.getByTestId("heading");
 
-    expect(headingEl).toHaveTextContent("Hello world");
+    expect(headingEl).toHaveTextContent("Hello World");
   });
 });
